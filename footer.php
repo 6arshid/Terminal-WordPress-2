deb56baa87c7cdcc47c87c63149773e15701d4e6
6arshid <51836304+6arshid@users.noreply.github.com>
2025-06-15 15:00:41 +0200

    Add custom logo and hamburger sidebar menu

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 </footer>
 
 <script src="<?php echo esc_url( get_template_directory_uri() . '/assets/main.js' ); ?>"></script>
+<script src="<?php echo esc_url( get_template_directory_uri() . '/assets/hamburger.js' ); ?>"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
